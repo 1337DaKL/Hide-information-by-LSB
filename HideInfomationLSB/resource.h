928334b802df163f9a32863844b4f2be7a33b8f5
1337DaKL <luongtrinh2k3ndad@gmail.com>
2026-01-30 22:44:34 +0700

    test LSB

---
+++ HideInfomationLSB/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by HideInfomationLSB.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_HIDEINFOMATIONLSB_DIALOG    102
+#define IDR_MAINFRAME                   128
+#define ID_SEPARATE_INFORMATION         1001
+#define ID_HIDE_INFORMATION             1002
+#define IDC_EDIT1                       1003
+#define INPUT_INFORMATION               1003
+#define IDC_MFCEDITBROWSE1              1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
